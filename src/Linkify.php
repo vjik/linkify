1f2ef653390a620c55deb299a3adad3d53e20550
Sergei Predvoditelev <sergey.predvoditelev@gmail.com>
2022-03-15 16:48:22 +0300

    Fix psalm

--- src/Linkify.php
+++ src/Linkify.php
@@ -29,15 +29,11 @@ final class Linkify
         /** @psalm-var array<string, string> $links */
         $links = [];
         foreach ($this->patterns as $pattern) {
-            /** @var string|null $result */
             $result = preg_replace_callback(
                 $pattern->getRegularExpression(),
-                function (array $match) use ($pattern, &$links) {
-                    /**
-                     * @psalm-var PatternInterface $pattern
-                     * @psalm-var array<string, string> $links
-                     */
-                    $plug = str_replace(self::VAR_ID, (string)count($links), $this->plugTemplate);
+                function (array $match) use ($pattern, &$links): string {
+                    /** @psalm-var array<string, string> $links */
+                    $plug = str_replace(self::VAR_ID, (string) count($links), $this->plugTemplate);
                     $links[$plug] = $pattern->callback($match);
                     return $plug;
                 },
